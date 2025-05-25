9e2878626d7290a084f15fe6fd7b202bdf7c736b
Raayan Dhar raayan.dhar@gmail.com <raayan.dhar@gmail.com>
2025-05-24 18:14:41 -0700

    fix training

--- script/train.sh
+++ script/train.sh
@@ -1,27 +1,33 @@
+#!/bin/bash
+
 DATA_PATH="/home/raayan/projects/SIGNature/datasets"
 
-# deepfake
-# python train_classifier.py --device_num 8 --per_gpu_batch_size 32 --total_epoch 50 --lr 2e-5 --warmup_steps 2000\
+# deepfake - Single GPU training
+# python train_classifier.py --device_num 1 --per_gpu_batch_size 32 --total_epoch 50 --lr 2e-5 --warmup_steps 2000\
 #     --model_name princeton-nlp/unsup-simcse-roberta-base --dataset deepfake --path ${DATA_PATH}/Deepfake/cross_domains_cross_models \
-#     --name deepfake-roberta-base --freeze_embedding_layer --database_name train --test_dataset_name test
+#     --name deepfake-roberta-base --freeze_embedding_layer --database_name train --test_dataset_name test \
+#     --use_signature --wandb_project "signature-deepfake"
 
-# # TuringBench
-python train_classifier.py --device_num 8 --per_gpu_batch_size 64 --total_epoch 50 --lr 2e-5 --warmup_steps 2000\
+# # TuringBench - Single GPU training
+python train_classifier.py --device_num 1 --per_gpu_batch_size 32 --total_epoch 50 --lr 2e-5 --warmup_steps 2000\
     --model_name princeton-nlp/unsup-simcse-roberta-base --dataset TuringBench --path ${DATA_PATH}/TuringBench/AA \
     --name TuringBench-roberta-base --freeze_embedding_layer --database_name train --test_dataset_name test \
     --use_signature --wandb_project "signature-turingbench"
 
-# # M4-monolingual
-# python train_classifier.py --device_num 8 --per_gpu_batch_size 32 --total_epoch 50 --lr 2e-5 --warmup_steps 2000\
+# # M4-monolingual - Single GPU training
+# python train_classifier.py --device_num 1 --per_gpu_batch_size 32 --total_epoch 50 --lr 2e-5 --warmup_steps 2000\
 #     --model_name princeton-nlp/unsup-simcse-roberta-base --dataset M4 --path ${DATA_PATH}/SemEval2024-M4/SubtaskA \
-#     --name M4-monolingual-roberta-base --freeze_embedding_layer --database_name monolingual_train --test_dataset_name monolingual_test
+#     --name M4-monolingual-roberta-base --freeze_embedding_layer --database_name monolingual_train --test_dataset_name monolingual_test \
+#     --use_signature --wandb_project "signature-m4-mono"
 
-# # M4-multilingual
-# python train_classifier.py --device_num 8 --per_gpu_batch_size 32 --total_epoch 50 --lr 2e-5 --warmup_steps 2000\
+# # M4-multilingual - Single GPU training
+# python train_classifier.py --device_num 1 --per_gpu_batch_size 32 --total_epoch 50 --lr 2e-5 --warmup_steps 2000\
 #     --model_name princeton-nlp/unsup-simcse-roberta-base --dataset M4 --path ${DATA_PATH}/SemEval2024-M4/SubtaskA \
-#     --name M4-multilingual-roberta-base --freeze_embedding_layer --database_name multilingual_train --test_dataset_name multilingual_test
+#     --name M4-multilingual-roberta-base --freeze_embedding_layer --database_name multilingual_train --test_dataset_name multilingual_test \
+#     --use_signature --wandb_project "signature-m4-multi"
 
-# # OUTFOX
-# python train_classifier.py --device_num 8 --per_gpu_batch_size 32 --total_epoch 50 --lr 2e-5 --warmup_steps 2000\
+# # OUTFOX - Single GPU training
+# python train_classifier.py --device_num 1 --per_gpu_batch_size 32 --total_epoch 50 --lr 2e-5 --warmup_steps 2000\
 #     --model_name princeton-nlp/unsup-simcse-roberta-base --dataset OUTFOX --path ${DATA_PATH}/OUTFOX \
-#     --name OUTFOX-roberta-base --freeze_embedding_layer --database_name train --test_dataset_name test
+#     --name OUTFOX-roberta-base --freeze_embedding_layer --database_name train --test_dataset_name test \
+#     --use_signature --wandb_project "signature-outfox"
